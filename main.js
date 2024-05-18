48e8bc381dbc32d167a0aac8d45faaadcccd715b
knwpound <142076222+knwpound@users.noreply.github.com>
2024-05-18 21:36:58 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const applyButton = document.getElementById('apply-button');
+    applyButton.addEventListener("click", function() {
+        handleSetting();
+            
+    });
+});
---
+++ setting.js
@@ -0,0 +1,6 @@
+export async function handleSetting() {
+    // Capacity
+    var fullCapacity = document.getElementById('full-capacity').value;
+    console.log(fullCapacity);
+    // get full capacity from <input type="number" name="Capacity" min="0">
+}
